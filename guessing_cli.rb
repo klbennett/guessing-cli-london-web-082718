287a229c5bd79b418009c408ffa108576e6a5993
Kay B <kaylouisebennett@gmail.com>
2018-08-24 10:42:56 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ until guess == "exit"
 answer = rand(1..6)
 puts "Guess a number between 1 and 6."
 guess = gets.chomp
-if guess == answer
+if guess.to_i == answer
 puts "You guessed the correct number!"
 elsif !(guess == answer)
 puts "The computer guessed #{answer}."
