dccf35b566d4ecf8cb31a14708d93e3a1afea744
Kay B <kaylouisebennett@gmail.com>
2018-08-24 10:23:46 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,18 +1,16 @@
-# Code your solution here!
 def run_guessing_game
+guess = nil
+while !(guess == "exit")
 answer = rand(1..6)
 puts "Guess a number between 1 and 6."
 guess = gets.chomp
-while !(guess == "exit")
-case guess
-when guess == answer
-prints  "You guessed the correct number!"
+if guess == answer
+puts "You guessed the correct number!"
 elsif !(guess == answer)
 puts "The computer guessed #{answer}."
 end
-prints "Goodbye!"
-break
 end
+puts "Goodbye!"
 end
 
 run_guessing_game
